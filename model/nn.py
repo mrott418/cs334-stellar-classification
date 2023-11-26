6b0d8ad64ced4598163b38f293a6cdebca57654d
rsriv26 <rishie.srivastava@emory.edu>
2023-11-25 21:43:47 -0500

    model and plot for nn

---
+++ model/nn.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+
+from util import tune_hyperparams, get_metrics
+
+# Hyperparameters to tune for NN
+grid_params = {
+    "hidden_layer_sizes": [(5, 4), (20, 10), (10, 20, 10), (10, 30, 20), (10, 30, 40, 20)],
+    "activation": ["identity", "logistic", "relu", "tanh"]
+}
+
+
+class NN(object):
+    def __init__(self):
+        # Tuned hyperparams: {'hidden_layer_sizes': (20, 10), 'activation': 'tanh'}
+        self.model = MLPClassifier(hidden_layer_sizes=[20, 10],
+                                   activation="tanh")
+
+    def tune(self, xTrain, yTrain):
+        params = tune_hyperparams(xTrain, yTrain, MLPClassifier(), grid_params)
+        print(params)
+        return params
+
+    def train(self, xTrain, yTrain, tune=False):
+        if tune:
+            self.tune(xTrain, yTrain)
+        print(self.model.fit(xTrain, yTrain))
+
+    def predict(self, xTest):
+        return self.model.predict(xTest)
+
+    def metrics(self, xTest, yTest):
+        return get_metrics(xTest, yTest, self.model)
+
+
+def main():
+    # load dataset
+    xTrain, yTrain, xTest, yTest = pd.read_csv("../data/xTrain.csv"), np.ravel(pd.read_csv("../data/yTrain.csv")), pd.read_csv(
+        "../data/xTest.csv"), np.ravel(pd.read_csv("../data/yTest.csv"))
+
+    model = NN()
+    model.train(xTrain, yTrain)
+
+    metrics = model.metrics(xTest, yTest)
+    print(metrics)
+    # Accuracy 0.97245
+
+
+if __name__ == "__main__":
+    main()
